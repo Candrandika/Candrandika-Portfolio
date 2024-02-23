e28ccd9eb512a5cd292001b4bb81da5e8c53ee25
Candrandika <candrandikarpl1.kanesa@gmail.com>
2024-02-23 16:24:20 +0700

    mod: add vercel speed insights

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,25 @@ import { AnimatePresence, motion } from "framer-motion"
 import Transition from "./components/Transition"
 import navData from "./components/NavData"
 import Layout from "./components/Layout"
-
+import { SpeedInsights } from "@vercel/speed-insights/react"
 function App() {
   const location = useLocation();
   return (
-    <Layout>
-      <AnimatePresence mode="wait">
-          <motion.div className="min-h-screen" key={location.key}>
-            <Transition/>
-            <Routes>
-              {navData.map((link, index) => {
-                return <Route key={index} path={link.path} element={link.element} />
-              })}
-            </Routes>
-          </motion.div>
-        </AnimatePresence>
-    </Layout>
+    <>
+      <Layout>
+        <AnimatePresence mode="wait">
+            <motion.div className="min-h-screen" key={location.key}>
+              <Transition/>
+              <Routes>
+                {navData.map((link, index) => {
+                  return <Route key={index} path={link.path} element={link.element} />
+                })}
+              </Routes>
+            </motion.div>
+          </AnimatePresence>
+      </Layout>
+      <SpeedInsights />
+    </>
   )
 }
 
